3552a52644b3ba78c80a948941f266d2e452a000
sarath <sasidharansharath13@gmail.com>
2025-07-21 21:55:05 +0530

    updated py

--- playwright.config.js
+++ playwright.config.js
@@ -26,6 +26,8 @@ export default defineConfig({
   reporter: [["html"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
